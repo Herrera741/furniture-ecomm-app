4bd972f33b28d8e3c040914dfc2a7888b1f9c163
Sergio Herrera <sherrera725@gmail.com>
2024-12-29 12:49:34 -0800

    add: implement bottom navigation bar
    - Implement bottom nav bar with animation
    - add Tab enum for different tab states
    - add TabButton() method
    - add extension for View for getting safe area size
    - add struct for blur UI VFX

--- furniture-ecomm-app/ContentView.swift
+++ furniture-ecomm-app/ContentView.swift
@@ -7,15 +7,139 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum Tab: String, CaseIterable {
+    case Home = "house"
+    case Search = "magnifyingglass.circle"
+    case Notifications = "bell"
+    case Cart = "bag"
+    case Profile = "person"
+    
+    var Tabname: String {
+        switch self {
+        case .Home:
+            return "Home"
+        case .Search:
+            return "Search"
+        case .Notifications:
+            return "Notifications"
+        case .Cart:
+            return "Cart"
+        case .Profile:
+            return "Profile"
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        // hide default UI tab appearance
+        UITabBar.appearance().isHidden = true
+    }
+    
+    // coordinated animation involving matched geo effects; used for smooth transitions between diff layouts or views
+    @Namespace var animation
+    
+    var body: some View {
+        TabView(selection: $currentTab) {
+            Text("Home View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Home)
+            Text("Search View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Search)
+            Text("Notifications View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Notifications)
+            Text("Cart View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Cart)
+            Text("Profile View")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background()
+                .tag(Tab.Profile)
+        }
+        .overlay(
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    TabButton(tab: tab)
+                }
+                .padding(.vertical)
+                .padding(.bottom, getSafeArea().bottom == 0 ? 5 : (getSafeArea().bottom - 15))
+                .background(Color("kSecondary"))
+            }
+            ,
+            alignment: .bottom
+        )
+        .ignoresSafeArea(.all, edges: .bottom)
+    }
+    
+    func TabButton(tab: Tab) -> some View {
+        GeometryReader { proxy in
+            Button {
+                withAnimation(.spring()) {
+                    currentTab = tab
+                }
+            } label: {
+                VStack(spacing: 0) {
+                    Image(systemName: currentTab == tab ? tab.rawValue + ".fill": tab.rawValue)
+                        .resizable()
+                        .foregroundStyle(Color("kPrimary"))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            ZStack {
+                                if currentTab == tab {
+                                    MaterialEffect(style: .light)
+                                        .clipShape(Circle())
+                                        .matchedGeometryEffect(id: "Tab", in: animation)
+                                    Text(tab.Tabname)
+                                        .foregroundStyle(.primary)
+                                        .font(.footnote)
+                                        .padding(.top, 50)
+                                }
+                            }
+                        )
+                        .contentShape(Rectangle())
+                        .offset(y: currentTab == tab ? -15 : 0)
+                }
+            }
+        }
+        .frame(height: 25)
+    }
+}
+
+extension View {
+    func getSafeArea() -> UIEdgeInsets {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .zero
+        }
+        
+        guard let safeArea = screen.windows.first?.safeAreaInsets else {
+            return .zero
+        }
+        
+        return safeArea
+    }
+}
+
+struct MaterialEffect: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        
     }
 }
 
